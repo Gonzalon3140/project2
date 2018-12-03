ea03b8dd8e753fcb6ad04d8222c768e635bcb1fe
Kam Sah <ksahrakar@gmail.com>
2018-12-03 05:28:43 -0800

    additional oauth files

--- config/oAuth.js
+++ config/oAuth.js
@@ -1,10 +1,12 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth");
+const keys = require("./keys")
 
 passport.use(
     new GoogleStrategy({
         // options for the Google Strategy
-        
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }),
     ()=>{
         // passport callback function
