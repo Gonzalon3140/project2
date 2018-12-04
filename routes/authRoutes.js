713ba7d3af4e0b36fc3bee933e78bbff30d2f4a4
Kam Sah <ksahrakar@gmail.com>
2018-12-03 16:35:51 -0800

    more auth work

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,23 @@
 // Export not working, so files temporarily in htmlRoutes.js file
 var passport= require("passport");
+var router = require("express").Router();
 
-module.exports = function(app) {
-  app.get("/login", function(req, res) {
-    res.render("login");
-  });
-  
-  app.get("/google", passport.authenticate("google",{
-    scope:["profile"]
-  }));
 
-  app.get("/logout", function(req, res) {
-    //handle with passport
-    res.send("Logging out...");
-  });
+router.get("/login", function(req, res) {
+  res.render("login");
+});
 
-  app.get("/google/redirect", function(req,res){
-    res.send("Callback URI reached succesfully");
-  });
-}
+router.get("/google", passport.authenticate("google",{
+  scope:["profile"]
+}));
+
+router.get("/logout", function(req, res) {
+  //handle with passport
+  res.send("Logging out...");
+});
+
+router.get("/google/redirect", function(req,res){
+  res.send("Callback URI reached succesfully");
+});
+
+module.exports = router;
