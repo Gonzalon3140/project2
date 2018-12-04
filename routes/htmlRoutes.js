8eaf2a95753698c6b7bf6dbf68e3f6fe525ed43f
Lindsay Terry <lterry.development@gmail.com>
2018-12-03 19:16:43 -0800

    lt update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,21 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // Load index/log-in page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");  
   });
 
+  // Load signup page
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
+
+  // Load home page
+  app.get("/home", function(req, res) {
+    res.render("home");
+  });
+    
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
