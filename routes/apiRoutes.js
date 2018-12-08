cb7380bcfaf1909e9993f5f8bd6f795f64442f13
Kyle Merryman <33881650+kyle-merryman@users.noreply.github.com>
2018-12-07 20:44:51 -0800

    add tags, functionality
    I've had trouble sorting out the functionality required to get an element by an id, especially with selecting a category, that corresponds to the database. I will need help from TA to sort this out further.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,56 @@
 var db = require("../models");
 var moment = require("moment");
+var authentic = require("../views/index.handlebars")
 
 module.exports = function (app) {
   /*---------------PAGE-POPULATOR----------------*/
 
+  //var to find by zipcode
+  var location = authentic.location;
+
   // LOAD HOMEPAGE w/ all your posts and 10 most recent posts in your area
   app.get("/api/:zip", function (req, res) {
+
+    // get 10 recent posts in user region
+    db.Posts.findAll({ 
+      limit: 10, 
+      where: {
+        zip: location
+      }
+    }).then(function (recent) {
+      for (i = 0; i < recent.length; i++) {
+        res.render("home", {
+          localPost: recent[i],
+          localTitle: recent[i].title,
+          localBody: recent[i].body,
+          postID: recent[i].id
+        });
+      }
+    });    
     // get all user's posts
     db.Posts.findAll({
       where: {
-        zip: req.params.zip
+        zip: location
       }
     }).then(function (userposts) {
-      res.render("home", {
-        userfeed: userposts
-      });
+      for (i = 0; i < userposts.length; i++) {
+        res.render("home", {
+          yourPosts: userposts[i],
+          yourTitle: userposts[i].title,
+          yourBody: userposts[i].title,
+          postID: userposts[i].id
+        });
+      }
     });
-    // get all recent posts in user region
-    db.Posts.findAll({
-      where: {
-        zip: req.params.zip
-      } /* && limited by most recent */
-    }).then(function (recent) {
-      res.render("home", {
-        localfeed: recent
-      });
-    });
+
   });
 
   // LOAD CATEGORY page containing all posts
   app.get("/api/:zip/:category", function (req, res) {
     db.Posts.findAll({
       where: {
-        category: req.params.category,
-        zip: req.params.zip
+        category: req.body.category,
+        zip: location
       }
     }).then(function (resPosts) {
       res.render("category", {
@@ -49,9 +66,9 @@ module.exports = function (app) {
     // Add sequelize code for creating a post using req.body,
 
     db.postTable.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category,
+      title: req.body.newTitle,
+      body: req.body.newBody,
+      category: req.body.data-id,
       expirationDate: moment().add(3, 'days').calendar() // will change based on category, set to 3 days
     }).then(function(response) {
       // then return the result using res.json
@@ -60,23 +77,25 @@ module.exports = function (app) {
 
   });
 
-  // DELETE ONE OF YOUR POSTS
-  // app.delete("/api/posts/:id", function (req, res) {
-  //   // Add sequelize code to delete a post where the id is equal to req.params.id,
-  //   db.postTable
-  //     .destroy({
-  //       where: {
-  //         id: req.params.id
-  //       }
-  //     })
-  //     .then(function (response) {
-  //       // then return the result using res.json
-  //       res.json(response);
-  //     });
-  // });
+  //DELETE ONE OF YOUR POSTS
+     app.delete("/api/posts/:id", function (req, res) {
+        var id = $(this).data("id");
+       // Add sequelize code to delete a post where the id is equal to req.params.id,
+       db.postTable
+         .destroy({
+           where: {
+             id: id
+           }
+         })
+         .then(function (response) {
+           // then return the result using res.json
+           res.json(response);
+         });
+     });
 
   // UPDATE YOUR POST
-  app.put("/api/posts", function (req, res) {
+  app.put("/api/posts/update", function (req, res) {
+    var id = $(this).data("id");
     // Add code here to update a post using the values in req.body, where the id is equal to
     db.postTable
       .update({
@@ -84,7 +103,7 @@ module.exports = function (app) {
         body: req.body.body
       }, {
         where: {
-          id: req.body.id
+          id: id
         }
       })
       .then(function (response) {
@@ -110,7 +129,6 @@ module.exports = function (app) {
         res.json(response);
       });
   });
-
 
   
   // DELETE ONE OF YOUR COMMENTS
@@ -149,16 +167,26 @@ module.exports = function (app) {
   // ADD USER ACCOUNT
   app.post("/api/users", function (req, res) {
     // Add sequelize code for creating a post using req.body,
+    var pass1 = $("#pass");
+    var pass2 = $("#confirm")
 
-    db.userTable.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,//check KAMRAN'S AUTHENTIFICATION
-      zipcode: req.body.zipcode
-    }).then(function(response) {
-      // then return the result using res.json
-      res.json(response);
-    });
+    if (pass1 == pass2) {
+      console.log("passwords identical");
+      db.userTable.create({
+        firstName: $("#first"),
+        lastName: $("#last"),
+        email: $("#email"),
+        password: $("#pass"),
+        zipcode: $("#zip"),
+      }).then(function(response) {
+        // then return the result using res.json
+        res.json(response);
+      });
+
+    } else { 
+      console.log("not identical");
+      alert("passwords are not identical, please re-enter");
+    }
 
   });
 };
